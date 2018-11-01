9c606189161a0f8c4d4f52c4ee6bcf005b1e36bc
Guilherme Oliveira <44686070+guilherme0411@users.noreply.github.com>
2018-11-01 20:41:57 -0300

    Create guilherme.sh

---
+++ guilherme.sh
@@ -0,0 +1,16 @@
+  #!/bin/bash
+  #Meu script de teste 
+  
+  #Nome do computador
+  hostname
+  read
+  
+  #Data e hora do computador
+  date
+  read
+  
+  #Rede do computador
+  ip address
+  read
+  
+  
