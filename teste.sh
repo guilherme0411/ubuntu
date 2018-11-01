6b3dd04a583e92dc2dbb33ce3a5b3dc46aadf71b
Guilherme Oliveira <44686070+guilherme0411@users.noreply.github.com>
2018-11-01 20:10:01 -0300

    Create teste.sh

---
+++ teste.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#Meu Primeiro Script
+
+#Listando os diretórios do Linux
+ls -lha /
+read
+clear
+
+#Atualizando o Apt ou Apt-Get
+apt update
+echo "Listas atualizadas!!!"
+read
+cls
+
+#Desligando o servidor
+shutdown -h now
